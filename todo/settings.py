c525bcbe84d13d6bed9b7aef0f51e158db931668
sarina-loli <sarina123getu@gmail.com>
2026-06-25 01:11:30 -0700

    commit

--- todo/settings.py
+++ todo/settings.py
@@ -117,15 +117,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 LOGIN_REDIRECT_URL = 'dashboard'
 
 LOGOUT_REDIRECT_URL = 'home'
