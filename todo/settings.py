49058f81c947eab2112de0702d4c8f7066a91464
sarina-loli <sarina123getu@gmail.com>
2026-07-10 11:28:54 -0700

    database add

--- todo/settings.py
+++ todo/settings.py
@@ -83,20 +83,14 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 import os
 import dj_database_url
 
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+import os
+import dj_database_url
+
+DATABASES = {
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
